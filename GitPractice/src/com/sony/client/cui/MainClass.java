92e68aab2ee0dc7e990948db926b60db90997087
Vishwanathpd <vishwanathdalawai321@gmail.com>
2019-09-17 12:16:40 +0530

    Username

--- GitPractice/src/com/sony/client/cui/MainClass.java
+++ GitPractice/src/com/sony/client/cui/MainClass.java
@@ -3,14 +3,7 @@ package com.sony.client.cui;
 public class MainClass {
 
 	public static void main(String[] args) {
-
-		
-System.out.println("namrata");
-
-System.out.println("deekshith kamath");
-
-
-
+		System.out.println("namrata");
+		System.out.println("deekshith kamath");
 	}
-
 }
